671c7f2541006a16805f9523207fcf00401677a9
lucas <lucasaita4000@gmail.com>
2025-09-03 03:55:49 -0300

    initial commit

---
+++ src/main/java/dev/lucas/HistoricalArtifactsEncyclopedia/HistoricalArtifactsEncyclopediaApplication.java
@@ -0,0 +1,13 @@
+package dev.lucas.HistoricalArtifactsEncyclopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalArtifactsEncyclopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricalArtifactsEncyclopediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lucas/HistoricalArtifactsEncyclopedia/HistoricalArtifactsEncyclopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucas.HistoricalArtifactsEncyclopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricalArtifactsEncyclopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
