8bb64489f0f68ee58673ea813b97431602c617d9
lucas <lucasaita4000@gmail.com>
2025-09-09 03:52:03 -0300

    feat: add annotation @Builder to work with the entity in Mapper

--- src/main/java/dev/lucas/HistoricalArtifactsEncyclopedia/entity/HistoricPerson.java
+++ src/main/java/dev/lucas/HistoricalArtifactsEncyclopedia/entity/HistoricPerson.java
@@ -1,10 +1,7 @@
 package dev.lucas.HistoricalArtifactsEncyclopedia.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -14,6 +11,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class HistoricPerson {
 
     @Id
