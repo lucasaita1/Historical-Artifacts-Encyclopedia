841130fbaa7dc04416ac236106502674941fb82d
lucas <lucasaita4000@gmail.com>
2025-09-10 03:53:53 -0300

    feat: add atribute id in HistoricMoment class

--- src/main/java/dev/lucas/HistoricalArtifactsEncyclopedia/entity/HistoricMoment.java
+++ src/main/java/dev/lucas/HistoricalArtifactsEncyclopedia/entity/HistoricMoment.java
@@ -1,7 +1,6 @@
 package dev.lucas.HistoricalArtifactsEncyclopedia.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +13,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class HistoricMoment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String moment;
 }
